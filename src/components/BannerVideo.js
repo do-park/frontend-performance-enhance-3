3e4a645fdf0176fcb24ac227c22a37db8feae06b
do-park <doheepa@kakao.com>
2023-01-07 21:25:41 +0900

    # 폰트 최적화
    FOUT(Flash of Unstyled Text) / FOIT(Flash of Invisible Text)
    1. 폰트 적용 시점 제어 - fontfaceobserver 라이브러리
    2. 폰트 사이즈 줄이기 - ttf 대신 woff/woff2

--- src/components/BannerVideo.js
+++ src/components/BannerVideo.js
@@ -1,7 +1,18 @@
-import React from 'react'
+import React, {useEffect, useState} from 'react'
 import video from '../assets/banner-video.mp4'
+import FontFaceObserver from 'fontfaceobserver';
+
+const font = new FontFaceObserver('BMYEONSUNG');
 
 function BannerVideo() {
+	const [isFontLoaded, setIsFontLoaded] = useState(false);
+
+	useEffect(() => {
+		font.load(null, 20000).then(function () {
+			console.log('font has loaded');
+			setIsFontLoaded(true);
+		})
+	}, [])
 	return (
 		<div className="BannerVideo w-full h-screen overflow-hidden relative bg-texture">
 			<div className="absolute h-screen w-full left-1/2">
@@ -13,7 +24,8 @@ function BannerVideo() {
 				*/}
 				<video src={video} className="absolute translateX--1/2 h-screen max-w-none min-w-screen -z-1 bg-black min-w-full min-h-screen" autoPlay loop muted/>
 			</div>
-			<div className="w-full h-full flex justify-center items-center">
+			<div className="w-full h-full flex justify-center items-center"
+				 style={{opacity: isFontLoaded ? 1 : 0, transition: 'opacity 0.3s ease'}}>
 				<div className="text-white text-center">
 					<div className="text-6xl leading-none font-semibold">KEEP</div>
 					<div className="text-6xl leading-none font-semibold">CALM</div>
